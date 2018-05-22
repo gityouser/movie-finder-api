8298d63777f63fa98b582a106434a98d793ce05a
Andrei Constantinescu <andreiconstantinescu@Andrei-Constantinescu.local>
2018-05-22 18:52:58 +0300

    Experimental

--- snippet.js
+++ snippet.js
@@ -9,13 +9,24 @@ let dataObj = fetch("https://jsonmock.hackerrank.com/api/movies/search/")
 
     
 async function pageReder(value) {
+    if(container.children.length > 0) {container.innerHTML = ''} 
     const rawData = await dataObj
     const movies = 
         rawData.data
         .filter(obj => obj.Type === "movie")
 
-    let filtered = movies
-        .map(movie => movie.Title)
-        .filter((title) => title.substring(0, value.length) === value)
-    console.log(filtered);
+    let filteredMovies = movies
+        .filter((movie) => movie.Title.substring(0, value.length) === value)
+
+    console.log(filteredMovies);
+    filteredMovies.forEach(movie => createCards(movie))
+}
+
+const createCards = (movie) => {
+    let div = document.createElement('div')
+    let img = document.createElement('img')
+    img.src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg"
+    container.appendChild(div)
+    div.appendChild(img)
+    div.innerHTML="MIMI"
 }
