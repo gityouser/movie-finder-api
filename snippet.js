32bbf173057615f0227c137abe0320a6c819c836
Andrei Constantinescu <andreiconstantinescu@Andrei-Constantinescu.local>
2018-05-23 15:31:17 +0300

    cards

--- snippet.js
+++ snippet.js
@@ -9,24 +9,36 @@ let dataObj = fetch("https://jsonmock.hackerrank.com/api/movies/search/")
 
     
 async function pageReder(value) {
-    if(container.children.length > 0) {container.innerHTML = ''} 
+    if(container.children.length >= 0) {removeChildren(container)}     
+    
     const rawData = await dataObj
     const movies = 
         rawData.data
         .filter(obj => obj.Type === "movie")
 
     let filteredMovies = movies
-        .filter((movie) => movie.Title.substring(0, value.length) === value)
+        .filter((movie) => movie.Title.substring(0, value.length).toLowerCase() === value.toLowerCase())
 
-    console.log(filteredMovies);
     filteredMovies.forEach(movie => createCards(movie))
+    console.log(filteredMovies);
+    console.log("Value length: ", value.length)
+    if(value.length < 1) {removeChildren(container)}    
 }
-
+function removeChildren(node) {
+    while(node.firstChild) {node.removeChild(node.firstChild)}
+}
 const createCards = (movie) => {
+    
     let div = document.createElement('div')
     let img = document.createElement('img')
-    img.src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg"
+    console.log(movie)
+    movie.Poster !== "N/A"
+     ? img.src=movie.Poster
+     : img.src="http://wonkville.net/wp-content/uploads/2016/04/No-image-available.jpg"
+
     container.appendChild(div)
     div.appendChild(img)
-    div.innerHTML="MIMI"
+    img.style.cssText="width:100px; height:150px"
 }
+
+
